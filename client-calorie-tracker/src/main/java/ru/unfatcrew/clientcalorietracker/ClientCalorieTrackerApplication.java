99b279a479a6198b465898034e08c434b6747beb
vladlukynov <lukynov.1@yandex.ru>
2023-11-03 18:50:43 +0300

    Make initial SpringMVC project
    Issue #1

---
+++ client-calorie-tracker/src/main/java/ru/unfatcrew/clientcalorietracker/ClientCalorieTrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.unfatcrew.clientcalorietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCalorieTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCalorieTrackerApplication.class, args);
+	}
+
+}
