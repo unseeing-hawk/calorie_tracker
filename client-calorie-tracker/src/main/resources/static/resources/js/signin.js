4ef8853850bcc075781834b707432c461afddb61
famv2000 <nhandk54@gmail.com>
2023-11-17 09:15:59 +0300

    Add validation to the authorization page

--- client-calorie-tracker/src/main/resources/static/resources/js/signin.js
+++ client-calorie-tracker/src/main/resources/static/resources/js/signin.js
@@ -1,47 +1,90 @@
+function setAnimationError(el) {
+    el.style.borderColor = "#C9544D"
+    el.style.animation = 'shake 0.5s'
+    setTimeout(() => {
+        el.style.animation = ''
+    }, 510);
+}
+
+var usernameElement = document.querySelector('#username')
+var passwordElement = document.querySelector('#password')
+
 var loginButton = document.querySelector('button[class="btn"]')
 loginButton.onclick = registerAccount
 function registerAccount(e) {
-    var usernameElement = document.querySelector('#username')
-    var passwordElement = document.querySelector('#password')
-    var labelError = document.querySelector("#error-input")
-    if(usernameElement.value.length === 0) {
-        labelError.style.display = 'none'
-        usernameElement.style.borderColor  = "red";
-        usernameElement.style.animation = "shake 0.5s"
-        setTimeout(function() {
-            usernameElement.style.animation = ''
-        }, 510)
+    let usernamErrorLabel = document.querySelector('#username-error-label')
+    let passwordErrorLabel = document.querySelector("#password-error-label")
+    
+    // input username
+    if(usernameElement.value.length === 0 || usernameElement.value.trim().length == 0) {
+        usernamErrorLabel.style.display = 'none'
+        passwordErrorLabel.style.display = 'none'
+        setAnimationError(usernameElement)
+        usernameElement.value = ""
+    }
+    else if (usernameElement.value.length < 8){
+        usernamErrorLabel.style.display = 'block'
+        usernamErrorLabel.innerHTML = "Username must contain 8 characters or more!"
+        setAnimationError(usernameElement)
+        setAnimationError(usernamErrorLabel)
     }
     else {
         usernameElement.style.borderColor  = "rgba(54, 47, 47, 0.4)"
+        usernamErrorLabel.style.display = 'none'
     }
+
+    // input password
     if(passwordElement.value.length === 0) {
-        labelError.style.display = 'none'
-        passwordElement.style.borderColor  = "red";
-        passwordElement.style.animation = "shake 0.5s"
-        setTimeout(function() {
-            passwordElement.style.animation = ''
-        }, 510)
+        passwordErrorLabel.style.display = 'none'
+        setAnimationError(passwordElement)
+    }
+    else if (passwordElement.value.length < 8){
+        passwordErrorLabel.style.display = 'block'
+        passwordErrorLabel.innerHTML = "Password must contain 8 characters or more!"
+        setAnimationError(passwordElement)
+        setAnimationError(passwordErrorLabel)
     }
     else {
+        passwordErrorLabel.style.display = 'none'
         passwordElement.style.borderColor  = "rgba(54, 47, 47, 0.4)";
     }
-    if(usernameElement.value.length !== 0 && passwordElement.value.length !== 0){
-        
-        if(usernameElement.value !== "admin" || passwordElement.value !== "123456" )
-        {
-            labelError.style.display = "block"
-            labelError.style.animation = "shake 0.5s"
-            setTimeout(function() {
-                labelError.style.animation = ''
-            }, 510)
+    
+    if(usernameElement.value.trim().length >= 8 && passwordElement.value.length >= 8){
+        usernamErrorLabel.style.display = 'none'
+        let notCorrectFormatUsername = !(/^[0-9a-zA-Z\_\.]+$/.test(usernameElement.value)) || !(/[a-zA-Z]/.test(usernameElement.value))
+        let notCorrectFormatPassword = (/^\s|\s$/.test(passwordElement.value)) || !(/^[a-zA-Z0-9\s\,\.\/\<\>\?\;\'\:\"\[\]\{\}\=\-\\\+\_\)\(\*\&\^\%\$\#\@\!\~\`\)]*$/.test(passwordElement.value))
+        if (notCorrectFormatUsername || notCorrectFormatPassword) {
+            passwordErrorLabel.style.display = 'block'
+            passwordErrorLabel.innerHTML = "Incorrect username or password!"
+            setAnimationError(passwordErrorLabel)
+        }
+        else {
+            // Get data from database and check passwords and accounts
+            // Try input with given username and password.
+            if(usernameElement.value !== "admin123456" || passwordElement.value !== "12345678"){
+                passwordErrorLabel.style.display = 'block'
+                passwordErrorLabel.innerHTML = "Incorrect username or password!"
+                setAnimationError(passwordErrorLabel)
+            }
         }
     }
 }
 
-document.addEventListener("keydown", function(event){
+usernameElement.addEventListener('keypress', function(event) {
     if(event.code === "Enter") {
         registerAccount()
+    }
+    if(event.target.value.length > 29) {
+        event.preventDefault()
+    }
+})
+
+passwordElement.addEventListener('keypress', function(event) {
+    if(event.code === "Enter") {
+        registerAccount()
+    }
+    if(event.target.value.length > 49) {
+        event.preventDefault()
     }
 })
 
