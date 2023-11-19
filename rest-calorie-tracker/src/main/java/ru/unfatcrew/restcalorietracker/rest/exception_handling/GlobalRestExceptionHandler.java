63e492fe433268df7c1dc2203d13728abfa3f0c6
Aleksei Stepanov <stepanovalex836@gmail.com>
2023-11-19 20:05:10 +0300

    Add validation configurator

---
+++ rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/rest/exception_handling/GlobalRestExceptionHandler.java
@@ -0,0 +1,32 @@
+package ru.unfatcrew.restcalorietracker.rest.exception_handling;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalRestExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse onConstraintValidationException(
+            ConstraintViolationException e) {
+        List<Violation> violations = e.getConstraintViolations().stream()
+                .map(
+                        violation -> new Violation(
+                                violation.getPropertyPath().toString(),
+                                violation.getMessage()
+                        )
+                )
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(HttpStatus.BAD_REQUEST.value(), violations);
+    }
+}
