5d7ace12d41c760b3b350604998d80c32f64de8b
Aleksei Stepanov <stepanovalex836@gmail.com>
2023-11-22 20:01:12 +0300

    Delete typos and give user_password a fixed size

--- db-data/init_db.sql
+++ db-data/init_db.sql
@@ -28,9 +28,9 @@ CREATE TABLE IF NOT EXISTS public.products
     "product_userId" bigint,
     product_name character varying(100) NOT NULL,
     product_calories integer NOT NULL,
-    "product_ proteins" numeric NOT NULL,
-    "product_ fats" numeric NOT NULL,
-    "product_ carbohydrates" numeric NOT NULL,
+    "product_proteins" numeric NOT NULL,
+    "product_fats" numeric NOT NULL,
+    "product_carbohydrates" numeric NOT NULL,
     "product_isActive" boolean NOT NULL,
     CONSTRAINT products_pkey PRIMARY KEY (product_id)
 );
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS public.users
     user_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
     user_name character varying(100) NOT NULL,
     user_login character varying(30) NOT NULL,
-    user_password text NOT NULL,
+    user_password character (68) NOT NULL,
     user_weight numeric NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (user_id)
 );
