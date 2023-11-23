6b85730354d8149f3d05bfe34e79ee9c3ec112b7
Aleksei Stepanov <stepanovalex836@gmail.com>
2023-11-23 21:30:37 +0300

    Delete unused dependencies

--- rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/dao/UserDAO.java
+++ rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/dao/UserDAO.java
@@ -1,7 +1,6 @@
 package ru.unfatcrew.restcalorietracker.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.unfatcrew.restcalorietracker.pojo.entity.User;
 
--- rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/rest/controller/UserRestController.java
+++ rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/rest/controller/UserRestController.java
@@ -1,12 +1,10 @@
 package ru.unfatcrew.restcalorietracker.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.unfatcrew.restcalorietracker.pojo.entity.User;
 import ru.unfatcrew.restcalorietracker.service.UserService;
 
-@Validated
 @RestController
 @RequestMapping("/users")
 public class UserRestController {
