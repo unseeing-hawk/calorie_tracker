91449f209fff3a1c1c483cbaa99e01210962b450
p.pankova <p.pankova@NB-3683.local>
2023-11-24 11:43:34 +0300

    fix dto classes according to classes Diagram

---
+++ rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/dto/ProductDTO.java
@@ -0,0 +1,110 @@
+package ru.unfatcrew.restcalorietracker.dto;
+
+public class ProductDTO {
+    private Long id;
+    private Long fatsecretId;
+    private String userLogin;
+    private String name;
+    private Integer calories;
+    private Float proteins;
+    private Float fats;
+    private Float carbohydrates;
+    private Boolean isActive;
+
+    public ProductDTO() {
+        this.id = 0L;
+        this.fatsecretId = 0L;
+        this.userLogin = "";
+        this.name = "";
+        this.calories = 0;
+        this.proteins = 0.0f;
+        this.fats = 0.0f;
+        this.carbohydrates = 0.0f;
+        this.isActive = false;
+    }
+
+    public ProductDTO(Long id, Long fatsecretId, String userLogin, String name, Integer calories, Float proteins,
+                      Float fats, Float carbohydrates, Boolean isActive) {
+        this.id = id;
+        this.fatsecretId = fatsecretId;
+        this.userLogin = userLogin;
+        this.name = name;
+        this.calories = calories;
+        this.proteins = proteins;
+        this.fats = fats;
+        this.carbohydrates = carbohydrates;
+        this.isActive = isActive;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public Long getFatsecretId() {
+        return fatsecretId;
+    }
+    
+    public String getUserLogin() {
+        return userLogin;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Integer getCalories() {
+        return calories;
+    }
+    
+    public Float getProteins() {
+        return proteins;
+    }
+    
+    public Float getFats() {
+        return fats;
+    }
+    
+    public Float getCarbohydrates() {
+        return carbohydrates;
+    }
+    
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFatsecretId(Long fatsecretId) {
+        this.fatsecretId = fatsecretId;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    public void setProteins(Float proteins) {
+        this.proteins = proteins;
+    }
+
+    public void setFats(Float fats) {
+        this.fats = fats;
+    }
+
+    public void setCarbohydrates(Float carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+}
