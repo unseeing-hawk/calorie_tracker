0dc4b2d119e08ac9838676df9526ccb52e7af61b
p.pankova <p.pankova@NB-3683.local>
2023-12-03 21:42:33 +0300

    add product functionality

--- rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/rest/controller/ProductRestController.java
+++ rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/rest/controller/ProductRestController.java
@@ -1,8 +1,13 @@
 package ru.unfatcrew.restcalorietracker.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ru.unfatcrew.restcalorietracker.pojo.dto.ProductPostDTO;
+import ru.unfatcrew.restcalorietracker.pojo.entity.Product;
 import ru.unfatcrew.restcalorietracker.service.ProductService;
 
 @RestController
@@ -14,5 +19,10 @@ public class ProductRestController {
     @Autowired
     public ProductRestController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody ProductPostDTO product) {
+        return productService.addProduct(product);
     }
 }
--- rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/service/ProductService.java
+++ rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/service/ProductService.java
@@ -1,16 +1,58 @@
 package ru.unfatcrew.restcalorietracker.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.validation.Valid;
+import ru.unfatcrew.restcalorietracker.dao.UserDAO;
 import ru.unfatcrew.restcalorietracker.dao.ProductDAO;
+import ru.unfatcrew.restcalorietracker.pojo.dto.ProductPostDTO;
+import ru.unfatcrew.restcalorietracker.pojo.entity.Product;
+import ru.unfatcrew.restcalorietracker.pojo.entity.User;
+import ru.unfatcrew.restcalorietracker.rest.exception_handling.exception.IllegalRequestArgumentException;
+import ru.unfatcrew.restcalorietracker.rest.exception_handling.validation.Violation;
 
 @Service
 public class ProductService {
 
+    private final UserDAO userDAO;
     private final ProductDAO productDAO;
 
     @Autowired
-    public ProductService(ProductDAO productDAO) {
+    public ProductService(UserDAO userDAO, ProductDAO productDAO) {
+        this.userDAO = userDAO;
         this.productDAO = productDAO;
+    }
+
+    @Transactional
+    public Product addProduct(@Valid ProductPostDTO productPostDTO) {
+        List<Violation> violationList = new ArrayList<>();
+        
+        User user = userDAO.findByLogin(productPostDTO.getUserLogin());
+        if (user == null) {
+            violationList.add(new Violation("addProduct.productPostDTO.userLogin",
+                    "not found"));
+        }
+
+        if (!violationList.isEmpty()) {
+            throw new IllegalRequestArgumentException(violationList);
+        }
+
+        Product product = new Product(0,
+                                    user,
+                                    productPostDTO.getName(),
+                                    productPostDTO.getCalories(),
+                                    productPostDTO.getProteins(),
+                                    productPostDTO.getFats(),
+                                    productPostDTO.getCarbohydrates(),
+                                    true);
+
+        productDAO.save(product);
+
+        return product;
     }
 }
