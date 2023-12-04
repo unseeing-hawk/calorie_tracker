2f021aeea74fa66db6d47437046cc4e66c259792
p.pankova <p.pankova@NB-3683.local>
2023-12-04 11:27:04 +0300

    fix column names in init_db script

--- db-data/init_db.sql
+++ db-data/init_db.sql
@@ -5,33 +5,33 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS public."mealTimes"
 (
-    "mealTime_id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
-    "mealTime_name" character varying(50) NOT NULL,
-    CONSTRAINT "mealTime_pkey" PRIMARY KEY ("mealTime_id")
+    "meal_time_id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+    "meal_time_name" character varying(50) NOT NULL,
+    CONSTRAINT "meal_time_pkey" PRIMARY KEY ("meal_time_id")
 );
 
 CREATE TABLE IF NOT EXISTS public.meals
 (
     meal_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
-    "meal_userId" bigint NOT NULL,
-    "meal_productId" bigint NOT NULL,
-    "meal_productWeight" numeric NOT NULL,
+    "meal_user_id" bigint NOT NULL,
+    "meal_product_id" bigint NOT NULL,
+    "meal_product_weight" numeric NOT NULL,
     meal_date date NOT NULL,
-    "meal_timeId" bigint NOT NULL,
+    "meal_time_id" bigint NOT NULL,
     CONSTRAINT meals_pkey PRIMARY KEY (meal_id)
 );
 
 CREATE TABLE IF NOT EXISTS public.products
 (
     product_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
-    "product_fatsecretId" bigint,
-    "product_userId" bigint,
+    "product_fatsecret_id" bigint,
+    "product_user_id" bigint,
     product_name character varying(100) NOT NULL,
     product_calories integer NOT NULL,
     "product_proteins" numeric NOT NULL,
     "product_fats" numeric NOT NULL,
     "product_carbohydrates" numeric NOT NULL,
-    "product_isActive" boolean NOT NULL,
+    "product_is_active" boolean NOT NULL,
     CONSTRAINT products_pkey PRIMARY KEY (product_id)
 );
 
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS public.users
 );
 
 ALTER TABLE IF EXISTS public.meals
-    ADD CONSTRAINT "meals_meal_productId_fkey" FOREIGN KEY ("meal_productId")
+    ADD CONSTRAINT "meals_meal_product_id_fkey" FOREIGN KEY ("meal_product_id")
     REFERENCES public.products (product_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -54,15 +54,15 @@ ALTER TABLE IF EXISTS public.meals
 
 
 ALTER TABLE IF EXISTS public.meals
-    ADD CONSTRAINT "meals_meal_timeId_fkey" FOREIGN KEY ("meal_timeId")
-    REFERENCES public."mealTimes" ("mealTime_id") MATCH SIMPLE
+    ADD CONSTRAINT "meals_meal_time_id_fkey" FOREIGN KEY ("meal_time_id")
+    REFERENCES public."mealTimes" ("meal_time_id") MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.meals
-    ADD CONSTRAINT "meals_meal_userId_fkey" FOREIGN KEY ("meal_userId")
+    ADD CONSTRAINT "meals_meal_user_id_fkey" FOREIGN KEY ("meal_user_id")
     REFERENCES public.users (user_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
@@ -70,7 +70,7 @@ ALTER TABLE IF EXISTS public.meals
 
 
 ALTER TABLE IF EXISTS public.products
-    ADD CONSTRAINT "users_userProducts" FOREIGN KEY ("product_userId")
+    ADD CONSTRAINT "users_user_products" FOREIGN KEY ("product_user_id")
     REFERENCES public.users (user_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
