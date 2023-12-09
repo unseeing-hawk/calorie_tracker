af0443f408c8093ecf12d8311286985b82c1f7a1
vladlukynov <lukynov.1@yandex.ru>
2023-12-09 23:52:21 +0300

    Disable credentials erasing

--- client-calorie-tracker/src/main/java/ru/unfatcrew/clientcalorietracker/security/SpringSecurityConfig.java
+++ client-calorie-tracker/src/main/java/ru/unfatcrew/clientcalorietracker/security/SpringSecurityConfig.java
@@ -1,13 +1,20 @@
 package ru.unfatcrew.clientcalorietracker.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+    @Autowired
+    public void disableEraseCredentials(AuthenticationManagerBuilder builder) {
+        builder.eraseCredentials(false);
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
