a64bbbf494470c31b84e7a8b4069e1c4638cbffd
p.pankova <p.pankova@NB-3683.local>
2023-12-24 01:36:49 +0300

    add logic for change and delete products

--- rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/service/ProductService.java
+++ rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/service/ProductService.java
@@ -1,7 +1,6 @@
 package ru.unfatcrew.restcalorietracker.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +11,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import jakarta.validation.Valid;
+
 import ru.unfatcrew.restcalorietracker.dao.UserDAO;
 import ru.unfatcrew.restcalorietracker.dao.ProductDAO;
+import ru.unfatcrew.restcalorietracker.pojo.dto.MealGetDataDto;
+import ru.unfatcrew.restcalorietracker.pojo.dto.MealGetDto;
+import ru.unfatcrew.restcalorietracker.pojo.dto.MealPutDataDto;
 import ru.unfatcrew.restcalorietracker.pojo.dto.ProductPostDTO;
+import ru.unfatcrew.restcalorietracker.pojo.dto.ProductPutDTO;
+import ru.unfatcrew.restcalorietracker.pojo.entity.Meal;
 import ru.unfatcrew.restcalorietracker.pojo.entity.Product;
 import ru.unfatcrew.restcalorietracker.pojo.entity.User;
+import ru.unfatcrew.restcalorietracker.pojo.request.ChangeProductsRequest;
+import ru.unfatcrew.restcalorietracker.pojo.response.ChangeMealsResponse;
+import ru.unfatcrew.restcalorietracker.pojo.response.ChangeProductsResponse;
 import ru.unfatcrew.restcalorietracker.rest.exception_handling.exception.IllegalRequestArgumentException;
+import ru.unfatcrew.restcalorietracker.rest.exception_handling.exception.ResourceNotFoundException;
 import ru.unfatcrew.restcalorietracker.rest.exception_handling.validation.Violation;
 
 @Service
@@ -71,6 +80,132 @@ public class ProductService {
         productDAO.save(product);
 
         return product;
+    }
+
+    @Transactional
+    public ChangeProductsResponse changeProducts(@Valid ChangeProductsRequest changeProductsRequest) {
+        List<Violation> violationList = new ArrayList<>();
+        List<ProductPutDTO> productsForChange = changeProductsRequest.getProductsForChange();
+        List<Long> productsIdsForDeletion = changeProductsRequest.getProductIdsForDeletion();
+
+        if (productsForChange.isEmpty() && productsIdsForDeletion.isEmpty()) {
+            violationList.add(new Violation("changeProducts.changeProductsRequest.productsForChange-productsIdsForDeletion",
+                    "cannot both be empty"));
+        }
+
+        if (!violationList.isEmpty()) {
+            throw new IllegalRequestArgumentException(violationList);
+        }
+        
+        String userLogin = changeProductsRequest.getUserLogin();
+        if (userDAO.findByLogin(userLogin) == null) {
+            violationList.add(new Violation("changeProducts.changeProductsRequest.user-login",
+                    "not found"));
+        }
+
+        if (!violationList.isEmpty()) {
+            throw new ResourceNotFoundException(violationList);
+        }
+
+        HashMap<Long, Product> productsForDeletionHashMap = new HashMap<>();
+        for (int i = 0; i < productsIdsForDeletion.size(); i++) {
+            if (productsForDeletionHashMap.containsKey(productsIdsForDeletion.get(i))) {
+                violationList.add(new Violation("changeProducts.changeProductsRequest.productsIdsForDeletion["
+                        + Integer.toString(i)
+                        + "].<list element>"
+                        , "already exists in productsIdsForDeletion"));
+            }
+
+            Product product = productDAO.findById(productsIdsForDeletion.get(i));
+            if (product == null) {
+                violationList.add(new Violation("changeProducts.changeProductsRequest.productsIdsForDeletion["
+                        + Integer.toString(i)
+                        + "].<list element>"
+                        , "not found"));
+            } 
+
+            if (product != null
+                    && !product.getUser().getLogin().equals(userLogin)) {
+                violationList.add(new Violation("changeProducts.changeProductsRequest.productsIdsForDeletion["
+                        + Integer.toString(i)
+                        + "].<list element>-userLogin"
+                        , "not connected"));
+            }
+
+            if (product != null) {
+                product.setActive(false);
+                productsForDeletionHashMap.put(product.getId(), product);
+            }
+        }
+
+        HashMap<Long, Product> productsForChangeHashMap = new HashMap<>();
+        for (int i = 0; i < productsForChange.size(); i++) {
+            ProductPutDTO productForChange = productsForChange.get(i);
+            long id = productForChange.getId();
+            if (productsForChangeHashMap.containsKey(id)) {
+                violationList.add(new Violation("changeProducts.changeProductsRequest.productsForChange["
+                        + Integer.toString(i)
+                        + "].id"
+                        , "already exists in productsForChange"));
+            }
+
+            if (productsForDeletionHashMap.containsKey(id)) {
+                violationList.add(new Violation("changeProducts.changeProductsRequest.productsForChange["
+                        + Integer.toString(i)
+                        + "].id"
+                        , "already exists in productsForDeletion"));
+            }
+
+            Product product = productDAO.findById(id);
+            if (product == null) {
+                violationList.add(new Violation("changeProducts.changeProductsRequest.productsForChange["
+                        + Integer.toString(i)
+                        + "].id"
+                        , "not found"));
+            }
+
+            if (product != null
+                    && !product.getUser().getLogin().equals(userLogin)) {
+                violationList.add(new Violation("changeProducts.changeProductsRequest.productsForChange["
+                        + Integer.toString(i)
+                        + "].id-userLogin"
+                        , "not connected"));
+            }
+
+            if (product != null) {
+                product.setName(productForChange.getName());
+                product.setCalories(productForChange.getCalories());
+                product.setProteins(productForChange.getProteins());
+                product.setFats(productForChange.getFats());
+                product.setCarbohydrates(productForChange.getCarbohydrates());
+                productsForChangeHashMap.put(id, product);
+            }
+        }
+
+        if (!violationList.isEmpty()) {
+            throw new ResourceNotFoundException(violationList);
+        }
+
+        List<ProductPutDTO> productsForChangeList = new ArrayList<>();
+        for (Map.Entry<Long, Product> entry : productsForChangeHashMap.entrySet()) {
+            Product product = entry.getValue();
+            productDAO.save(product);
+            productsForChangeList.add(new ProductPutDTO(product.getId(),
+                                                    product.getName(),
+                                                    product.getCalories(),
+                                                    product.getProteins(),
+                                                    product.getFats(),
+                                                    product.getCarbohydrates()));
+        }
+
+        List<Long> productsForDeletionList = new ArrayList<>();
+        for (Map.Entry<Long, Product> entry : productsForDeletionHashMap.entrySet()) {
+            Product product = entry.getValue();
+            productDAO.save(product);
+            productsForDeletionList.add(product.getId());
+        }
+
+        return new ChangeProductsResponse(productsForChangeList, productsForDeletionList);
     }
 
     public List<ProductPostDTO> getUserProducts(@Valid int limit, 
