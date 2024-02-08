41262e8d6b01d8a7a08f333eb02c6f30bb68f574
Aleksei Stepanov <stepanovalex836@gmail.com>
2024-02-08 18:33:15 +0300

    Remove redundant spaces

--- rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/pojo/entity/User.java
+++ rest-calorie-tracker/src/main/java/ru/unfatcrew/restcalorietracker/pojo/entity/User.java
@@ -6,7 +6,7 @@ import ru.unfatcrew.restcalorietracker.validation.annotation.FiniteFloat;
 
 @Entity
 @Table(name="users")
-public class    User {
+public class User {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
