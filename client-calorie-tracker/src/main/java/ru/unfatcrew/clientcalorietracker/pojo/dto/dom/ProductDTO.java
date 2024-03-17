dba85017d2fc4edbdf1b654bd2b672dc1e4191f8
vladlukynov <lukynov.1@yandex.ru>
2024-03-17 04:23:52 +0300

    Add equals and hashCode methods to ProductDTO DOM POJO

--- client-calorie-tracker/src/main/java/ru/unfatcrew/clientcalorietracker/pojo/dto/dom/ProductDTO.java
+++ client-calorie-tracker/src/main/java/ru/unfatcrew/clientcalorietracker/pojo/dto/dom/ProductDTO.java
@@ -4,6 +4,7 @@ import ru.unfatcrew.clientcalorietracker.pojo.entity.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ProductDTO {
     private List<Product> products;
@@ -33,5 +34,19 @@ public class ProductDTO {
 
     public void setIdsToDelete(List<Long> idsToDelete) {
         this.idsToDelete = idsToDelete;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        ProductDTO that = (ProductDTO) object;
+        return Objects.equals(products, that.products) &&
+                Objects.equals(idsToDelete, that.idsToDelete);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(products, idsToDelete);
     }
 }
